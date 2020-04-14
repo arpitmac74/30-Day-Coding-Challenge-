650ba81fc22424343191fb84d9fc9c3e55b8dbbf
Arpit Maclay <arpitmac74@gmail.com>
2020-04-14 11:45:36 -0400

    Day-2

---
+++ Maxsubarray.py
@@ -0,0 +1,15 @@
+#Given an integer array nums, find the contiguous subarray #(containing at least one number) which has the largest sum and #return its sum.
+
+
+
+class Solution:
+        def maxSubArray(self, A):
+            if not A:
+                return 0
+
+            curSum = maxSum = A[0]
+            for num in A[1:]:
+                curSum = max(num, curSum + num)
+                maxSum = max(maxSum, curSum)
+
+            return maxSum

e0b494640828cb3cb26cd4c568b977611368646e
Arpit Maclay <arpitmac74@gmail.com>
2020-06-25 12:42:24 -0400

    add two numbers

---
+++ Add_two_numbers_linked_list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        int carry =0;
+        
+        while(l1||l2||carry){
+            int val1 = l1?l1->val:0;
+            int val2 = l2?l2->val:0;
+            
+            temp->next =new ListNode((val1+val2+carry)%10);
+            temp = temp->next;
+            carry = (val1+val2+carry)/10;
+            if(l1){
+                l1=l1->next;
+            }
+            if(l2){
+                l2 = l2->next;
+            }  
+        }
+        return dummy->next;
+        
+        
+    }
+};
